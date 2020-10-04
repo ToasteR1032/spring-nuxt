4fb68cafd9eea39fb614fe78226b62b33742e859
Paul Kalauner <p.k@magic-consoles.com>
2020-10-04 15:24:19 +0200

    Fix pom

--- backend/src/main/java/at/toaster/springnuxt/configuration/WebSecurityConfig.java
+++ backend/src/main/java/at/toaster/springnuxt/configuration/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/v1/auth/**").permitAll()
                 .antMatchers("/api/v1/test/**").permitAll()
+                .antMatchers("/**/").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
